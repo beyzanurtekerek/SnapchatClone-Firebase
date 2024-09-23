2e426234aeaab9757a9ea3ad77d7edb37a9a5e26
Beyza Nur Tekerek <beyzanurtekerk@gmail.com>
2024-09-23 20:55:39 +0300

    Initial Commit

---
+++ SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapchatClone
+//
+//  Created by Beyza Nur Tekerek on 23.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
